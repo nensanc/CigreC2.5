15b93c2583c105c9a29d1ca1b7644621af6670c3
Msanchez1002 <lucysanenc@gmail.com>
2022-07-24 11:11:53 -0500

    se agrega django y django-rest-framwork

--- iacigre/src/App.js
+++ iacigre/src/App.js
@@ -1,11 +1,12 @@
 import {Provider} from 'react-redux';
 import store from './store';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Error404 from './helpers/Error';
 import Login from './containers/Login';
 import Home from './containers/Home';
-
+import About from './components/pages/About';
+import Faq from './components/pages/Faq';
 function App() {
   return (
     <Provider store={store}>
@@ -15,6 +16,8 @@ function App() {
             <Route path="*" element={<Error404/>}/>
             <Route exact path='/' element={<Login/>}/>
             <Route exact path='/home' element={<Home/>}/>
+            <Route exact path='/about' element={<About/>}/>
+            <Route exact path='/faq' element={<Faq/>}/>
 
           </Routes>
       </Router>
--- iacigre/src/components/navigation/Navbar.js
+++ iacigre/src/components/navigation/Navbar.js
@@ -1,17 +1,17 @@
 import { connect } from 'react-redux';
+import { Link } from "react-router-dom";
 
 function Navbar({
 }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container px-5">
-            <a className="navbar-brand" href="#">CIGRE C2.5</a>
+            <Link className="navbar-brand" to='/home'>Cigre C2.5</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li className="nav-item"><a className="nav-link" href="about.html">About</a></li>
-                    <li className="nav-item"><a className="nav-link" href="contact.html">Contact</a></li>
-                    <li className="nav-item"><a className="nav-link" href="pricing.html">Blog</a></li>
+                    <li className="nav-item"><Link className="nav-link" to='/about'>About</Link></li>
+                    <li className="nav-item"><Link className="nav-link" to='/faq'>FAQ</Link></li>                    
                 </ul>
             </div>
         </div>
